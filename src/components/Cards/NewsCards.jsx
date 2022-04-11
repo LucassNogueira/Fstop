d6da11ed8b3bcf67f47aadc820cbd4944ea3421d
LucassNogueira <simplylucasjason@gmail.com>
2022-04-11 11:38:56 -0500

    addign new news articles

--- src/components/Cards/NewsCards.jsx
+++ src/components/Cards/NewsCards.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import norris from "../media/norris.png";
-import ferrari from "../media/ferrari.png";
-import haas from "../media/haas.png";
+import norris from "../media/newsimg/norris.png";
+import ferrari from "../media/newsimg/ferrari.png";
+import haas from "../media/newsimg/haas.png";
+import russell from "../media/newsimg/russellnews.jpg";
+import alonso from "../media/newsimg/alonso.jpg";
+import drick from "../media/newsimg/drick.jpg";
+import double from "../media/newsimg/double.jpg";
+import pissed from "../media/newsimg/pissed.jpg";
+import match from "../media/newsimg/match.jpg";
+import aston from "../media/newsimg/aston.jpg";
+import merc from "../media/newsimg/merc.jpg";
+import fir from "../media/newsimg/fir.jpg";
 const posts = [
   {
     title: "Norris finally speaks up!",
@@ -43,6 +52,150 @@ const posts = [
     date: "Feb 12, 2020",
     datetime: "2020-02-12",
     imageUrl: haas,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: "Russell's first podium for Mercedes",
+    href: "#",
+
+    description: `George Russell took his first podium for Mercedes and second in the drivers’ standings with a P3 finish in the Australian Grand Prix – the Briton later admitting that he was surprised to be in that position, given his team’s deficit to Red Bull and Ferrari.`,
+    date: "Feb 12, 2022",
+    datetime: "2020-02-12",
+    imageUrl: russell,
+    readingTime: "11 min",
+    author: {
+      name: "Roel Aufderehar",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: "Alonso left 'speechless' after weekend in Melbourne",
+    href: "#",
+
+    description: `Fernando Alonso said he was “speechless” that he couldn’t challenge the Mercedes in Sunday’s Australian Grand Prix, given he believed Alpine’s pace to be superior to the Silver Arrows’. And the two-time champion said a run of misfortune cost him a possible podium at Albert Park.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: alonso,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: "Daniel Ricciardo stoked on not having a 'painful home race'!",
+    href: "#",
+
+    description: `Daniel Ricciardo was relieved to score his first points of the season and, moreover, accomplish that in front of his home fans at Albert Park given a rocky start to the season for McLaren.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: drick,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: `McLaren happy with double-points finish – but Norris warns repeats will be tough`,
+    href: "#",
+
+    description: `Having scored just six points from the season’s opening two rounds, McLaren came away from Melbourne with a further 18, after both their drivers finished in the top 10. Maintaining such results could be tough, however, according to Lando Norris.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: double,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: `‘If you want to fight for the title these things can’t happen’ – Verstappen `,
+    href: "#",
+
+    description: `Max Verstappen retired from his second Grand Prix in three races in Melbourne, losing out on what he said would have been an “easy P2” behind eventual winner Charles Leclerc – with the Dutchman left unamused by his Australian Grand Prix stoppage.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: pissed,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: `‘We couldn’t match Ferrari at any point’ says Perez after salvaging P2 for Red Bull`,
+    href: "#",
+
+    description: `On a Sunday in which Charles Leclerc cruised to victory for Ferrari and team mate Max Verstappen retired from contention, Sergio Perez said he accomplished the ‘maximum’ possible for Red Bull with P2 at Albert Park – especially given the Scuderia’s pace.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: match,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: `Vettel says Aston Martin was ‘too much of a handful for me’ after crashing out of racing return in Australia`,
+    href: "#",
+
+    description: `Sebastian Vettel says his race-ending crash in Melbourne was a result of him wanting “too much too soon” on his return to racing after missing the first two rounds of the season with Covid-19.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: aston,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: `Hamilton explains ‘difficult position’ radio message after coming home behind Russell in Melbourne`,
+    href: "#",
+
+    description: `Lewis Hamilton was full of praise for his team after Mercedes bagged something of a surprising P3 and P4 finish in Australia – and explained a late radio message heard on the TV broadcast that he insisted wasn’t a criticism of the Silver Arrows and his race strategy.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: merc,
+    readingTime: "11 min",
+    author: {
+      name: "Daniela Metz",
+      href: "#",
+      imageUrl:
+        "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  },
+  {
+    title: `‘It was the first race where we could control the gap’ says Leclerc after extending championship lead Down Under`,
+    href: "#",
+
+    description: `Charles Leclerc said he was able to control the lead of a race for the first time in F1 after a show of dominance in Melbourne, as the Ferrari driver extended his lead in the championship with a very comfortable victory in the Australian Grand Prix.`,
+    date: "Feb 12, 2020",
+    datetime: "2020-02-12",
+    imageUrl: fir,
     readingTime: "11 min",
     author: {
       name: "Daniela Metz",
