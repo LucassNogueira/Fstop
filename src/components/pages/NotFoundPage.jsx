2b8eca03a8e395abed2939533bf1a95f15e0fdec
LucassNogueira <simplylucasjason@gmail.com>
2022-04-13 16:46:46 -0500

    fixed footer

--- src/components/pages/NotFoundPage.jsx
+++ src/components/pages/NotFoundPage.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import img from "../media/puncture.jpg";
 const NotFoundPage = () => {
   return (
-    <div className="mt-16 border-2  relative w-full h-full">
-      <img
-        alt="puncture"
-        src={img}
-        className="w-full absolute h-[950px] -mt-[81px] object-cover"
-      />
+    <div className="mt-16 border-2  relative">
+      <img alt="puncture" src={img} className="w-full  h-[950px] -mt-[81px] " />
       <h1 className="absolute font-extrabold text-7xl top-80 left-40">
         Looks like you've reached the 404!
       </h1>
